a58d5a6f186c48486d0fb319b55b8976e6242f16
Peter Lohrmann <peterl@valvesoftware.com>
2013-04-01 15:08:44 -0800

    ApiTrace: more progress on state capturing * Separate out state that can be queried, but not replayed (ie, max's, versions, etc) * Separate out most state that was deprecated as of OpenGL 3.3, as we won't need to focus on those right now * Add a code path for simple state that can be queried with one glGet*, and set with one entrypoint * Fixed up Material and Light setting even though those are deprecated * A lot of progress on snapshot of texture state, however this is still incomplete as it does not handle textures that were created, but not part of the currently bound state
    [git-p4: depot-paths = "//console/devtools/ApiTrace/": change = 1721018]

--- wrappers/gltrace_state_snapshot.hpp
+++ wrappers/gltrace_state_snapshot.hpp
@@ -36,8 +36,7 @@ void snapshotState()
 
     glstate::snapshotParameters();
 
-    _trace_glClearColor(0,0,0,1, false);
-    _trace_glClear(GL_COLOR_BUFFER_BIT, false);
+    _trace_glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT, false);
     BOOL bResult = TRUE;
     bResult = _trace_wglSwapBuffers((HDC)pCurrentContext->hdc, bResult, false);
 }
